d0aa137a1cd8267ce9d3a2b1adf2d5ea84bf5994
tangqi <tangqi_1987@sina.com>
2019-04-26 10:37:25 +0800

    获取用户头像昵称接口 bug fix

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -18,54 +18,55 @@ use Illuminate\Routing\Controller as BaseController;
 
 /**
  * Class Controller
+ *
  * @package iBrand\Auth\Api\Controllers
  */
 abstract class Controller extends BaseController
 {
-    /**
-     * @param array $data
-     * @param int $code
-     * @param bool $status
-     *
-     * @return Response
-     */
-    public function success($data = [], $code = Response::HTTP_OK, $status = true)
-    {
-        return new Response(['status' => $status, 'code' => $code, 'data' => empty($data) ? null : $data]);
-    }
+	/**
+	 * @param array $data
+	 * @param int   $code
+	 * @param bool  $status
+	 *
+	 * @return Response
+	 */
+	public function success($data = [], $code = Response::HTTP_OK, $status = true)
+	{
+		return new Response(['status' => $status, 'code' => $code, 'data' => empty($data) ? null : $data]);
+	}
 
-    /**
-     * @param      $message
-     * @param int $code
-     * @param bool $status
-     *
-     * @return mixed
-     */
-    public function failed($message, $code = Response::HTTP_BAD_REQUEST, $status = false)
-    {
-        return new Response(['status' => $status, 'code' => $code, 'message' => $message]
-        );
-    }
+	/**
+	 * @param      $message
+	 * @param int  $code
+	 * @param bool $status
+	 *
+	 * @return mixed
+	 */
+	public function failed($message, $code = Response::HTTP_BAD_REQUEST, $status = false)
+	{
+		return new Response(['status' => $status, 'code' => $code, 'message' => $message]
+		);
+	}
 
+	/**
+	 * get user model by unionid.
+	 *
+	 * @param $unionid
+	 *
+	 * @return |null
+	 */
+	protected function getUserByUnionid($unionid)
+	{
+		$binds = app(UserBindRepository::class)->getByUnionId($unionid);
 
-    /**
-     * get user model by unionid.
-     * @param $unionid
-     * @return |null
-     */
-    protected function getUserByUnionid($unionid)
-    {
-        $binds = app(UserBindRepository::class)->getByUnionId($unionid);
+		$user = null;
 
-        $user = null;
+		$binds->each(function ($item, $key) use ($user) {
+			if ($item->user_id) {
+				$user = app(UserRepository::class)->find($item->user_id);
+			}
+		});
 
-        $binds->each(function ($item, $key) use ($user) {
-            if (!$item->user_id) {
-                $user = app(UserRepository::class)->find($item->user_id);
-            }
-        });
-
-        return $user;
-
-    }
+		return $user;
+	}
 }
